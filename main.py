df0f0a77a1e8dcdd108657bea24553067eec9a6e
SceneryMC <1330827323@qq.com>
2022-08-29 09:28:00 +0800

    nothing

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ with open(r"E:\共享\torrent_hash.txt", "r") as f:
     torrents = f.readlines()
 for torrent in torrents:
     torrent = torrent.strip()
-    shutil.copy(rf"E:\共享\torrents\{torrent}.torrent", r"E:\共享\dst")
-    shutil.copy(rf"E:\共享\torrents\{torrent}.fastresume", r"E:\共享\dst")
+    shutil.copy(rf"C:\Users\SceneryMC\AppData\Local\qBittorrent\BT_backup\{torrent}.torrent", r"E:\共享\dst")
+    shutil.copy(rf"C:\Users\SceneryMC\AppData\Local\qBittorrent\BT_backup\{torrent}.fastresume", r"E:\共享\dst")
