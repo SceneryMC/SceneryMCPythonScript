d10e5b7ad5fd544712715f96ddbe00d8f7458d40
SceneryMC <1330827323@qq.com>
2022-09-01 09:59:02 +0800

    encoding = utf-8

--- mm_switch_between_windows_and_linux.py
+++ mm_switch_between_windows_and_linux.py
@@ -3,12 +3,12 @@ from pdf_page_and_annot_linker import filelist, isLinux, address_in_platform, t1
 
 
 for file in filelist:
-    with open(address_in_platform(file[0]), 'r') as f:
+    with open(address_in_platform(file[0]), 'r', encoding="utf-8") as f:
         content = f.read()
     content = content.replace(t1[not isLinux], t1[isLinux])
     t2 = (urllib.request.quote(f'=OpenActions" "{address_in_platform(file[1], False)}"'), f' {address_in_platform(file[1], True)}')
     content = content.replace(t2[not isLinux], t2[isLinux])
-    with open(address_in_platform(file[0]), 'w') as f:
+    with open(address_in_platform(file[0]), 'w', encoding="utf-8") as f:
         f.write(content)
 # with open("/mnt/E/学习资料/计算机/参考书/可能会读的书/算法/算法导论/整理/CLRS.mm", 'r') as f:
 #     content_original = f.read()
