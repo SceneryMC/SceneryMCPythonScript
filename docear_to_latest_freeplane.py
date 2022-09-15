0e99455fddfd0169b16daa19ca733f526d6df69c
SceneryMC <1330827323@qq.com>
2022-09-15 17:17:36 +0800

    docear_to_latest_freeplane重现，功能加强+完全自动化！

--- docear_to_latest_freeplane.py
+++ docear_to_latest_freeplane.py
@@ -1,11 +1,8 @@
 import re
 import urllib.request
-import html
-from pdf_page_and_annot_linker import acrobat_address
+from pdf_page_and_annot_linker import acrobat_address, filelist
 
-mm_address = r'E:\学习资料\计算机\参考书\可能会读的书\计算机系统\操作系统概念\操作系统概念.mm'
-pdf_address = r'E:\学习资料\计算机\参考书\可能会读的书\计算机系统\操作系统概念\操作系统概念9th.pdf'
-hyperlink = html.escape(r"project://181BF1A7D813BUY4OAN8U6I27YKD5SVHBUYL/../操作系统概念.pdf")
+mm_address, pdf_address = filelist['机器学习']
 
 
 def add_cmd_command(match):
@@ -21,39 +18,8 @@ def add_cmd_command(match):
 with open(mm_address, encoding='utf-8') as f:
     mm_html_text = f.read()
 
-    mm_html_text = re.sub(rf'LINK="{hyperlink}"', add_cmd_command, mm_html_text)
+    mm_html_text = re.sub(rf'LINK="project://\w+/.+?\.pdf"', add_cmd_command, mm_html_text)
+    mm_html_text = re.sub(rf"\n<pdf_annotation .*?/>", "", mm_html_text, 0)
+    mm_html_text = re.sub(rf"\n<pdf_annotation .*?</pdf_annotation>", "", mm_html_text, 0, re.DOTALL)
 with open(mm_address, 'w', encoding='utf-8') as f:
     f.writelines(mm_html_text)
-
-# from PyPDF2 import PdfReader
-#
-# pdf_address = r"E:\学习资料\计算机\参考书\可能会读的书\算法\算法导论\3rd\算法导论（第3版）（中文版）[（美）Thomas.H.Cormen Charles.E.Leiserson Ronald.L.Rivest Clifford.Stein].pdf"
-# reader = PdfReader("example.pdf")
-#
-#
-# def _setup_page_id_to_num(pdf, pages=None, _result=None, _num_pages=None):
-#     if _result is None:
-#         _result = {}
-#     if pages is None:
-#         _num_pages = []
-#         pages = pdf.trailer["/Root"].getObject()["/Pages"].getObject()
-#     t = pages["/Type"]
-#     if t == "/Pages":
-#         for page in pages["/Kids"]:
-#             _result[page.idnum] = len(_num_pages)
-#             _setup_page_id_to_num(pdf, page.getObject(), _result, _num_pages)
-#     elif t == "/Page":
-#         _num_pages.append(1)
-#     return _result
-#
-#
-# def walk_bookmarks(elem, indent):
-#     if isinstance(elem, list):
-#         for sub_elem in elem:
-#             walk_bookmarks(sub_elem, indent + 1)
-#     else:
-#         print(f"{'    ' * indent}{elem['/Title']}:{pg_id_num_map[elem.page.idnum] + 1}")
-#
-#
-# pg_id_num_map = _setup_page_id_to_num(reader)
-# walk_bookmarks(reader.outlines, -1)
