c4872e68a87e58ae40bf1ec4a4f8527ae00ceeec
SceneryMC <1330827323@qq.com>
2022-09-21 20:24:18 +0800

    将filelist分出，优化程序结构

--- pdf_page_and_annot_linker.py
+++ pdf_page_and_annot_linker.py
@@ -4,54 +4,10 @@ import html
 import fitz
 import platform
 from error_correction_dictionary import character_error_correction, word_error_correction
+from mm_filelist import filelist
 
 isLinux = (platform.system().lower() == "linux")
-intersect_portion = 0.5
-filelist = {
-    "CLRS": (r"E:\学习资料\计算机\参考书\可能会读的书\算法\算法导论\CLRS.mm",
-             r"E:\学习资料\计算机\参考书\可能会读的书\算法\算法导论\CLRS4ed.pdf"),
-    "ITLA": (r"E:\学习资料\计算机\参考书\可能会读的书\数学\线性代数\ITLA\ITLA.mm",
-             r"E:\学习资料\计算机\参考书\可能会读的书\数学\线性代数\ITLA\ITLA5ed.pdf"),
-    "LADR": (r"E:\学习资料\计算机\参考书\可能会读的书\数学\线性代数\LADR\LADR.mm",
-             r"E:\学习资料\计算机\参考书\可能会读的书\数学\线性代数\LADR\LADR3edCN.pdf"),
-    "CppPrimer": (r"E:\学习资料\计算机\参考书\可能会读的书\Cpp\入门\CppPrimer\CppPrimer.mm",
-                  r"E:\学习资料\计算机\参考书\可能会读的书\Cpp\入门\CppPrimer\CppPrimer5edCN.pdf"),
-    "C程序设计语言": (r"E:\学习资料\计算机\参考书\可能会读的书\C\入门\C程序设计语言\C程序设计语言.mm",
-                    r"E:\学习资料\计算机\参考书\可能会读的书\C\入门\C程序设计语言\C程序设计语言2edCN.pdf"),
-    "CPrimerPlus": (r"E:\学习资料\计算机\参考书\可能会读的书\C\入门\CPrimerPlus\CPrimerPlus.mm",
-                    r"E:\学习资料\计算机\参考书\可能会读的书\C\入门\CPrimerPlus\CPrimerPlus6edCN.pdf"),
-    "C专家编程": (r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C专家编程\C专家编程.mm",
-                  r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C专家编程\C专家编程.pdf"),
-    "C标准库": (r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C标准库\C标准库.mm",
-                r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C标准库\C标准库.pdf"),
-    "C和指针": (r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C和指针\C和指针.mm",
-                r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C和指针\C和指针.pdf"),
-    "C陷阱与缺陷": (r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C陷阱与缺陷\C陷阱与缺陷.mm",
-                    r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C陷阱与缺陷\C陷阱与缺陷.pdf"),
-    "C语言参考手册": (r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C语言参考手册\C语言参考手册.mm",
-                    r"E:\学习资料\计算机\参考书\可能会读的书\C\进阶\C语言参考手册\C语言参考手册5ed.pdf"),
-    "深入理解计算机系统": (r"E:\学习资料\计算机\参考书\可能会读的书\计算机系统\深入理解计算机系统\深入理解计算机系统.mm",
-                        r"E:\学习资料\计算机\参考书\可能会读的书\计算机系统\深入理解计算机系统\深入理解计算机系统3edCN.pdf"),
-    "操作系统导论": (r"E:\学习资料\计算机\参考书\可能会读的书\计算机系统\操作系统导论\操作系统导论.mm",
-                     r"E:\学习资料\计算机\参考书\可能会读的书\计算机系统\操作系统导论\操作系统导论2019CN.pdf"),
-    "操作系统概念": (r"E:\学习资料\计算机\参考书\可能会读的书\计算机系统\操作系统概念\操作系统概念.mm",
-                     r"E:\学习资料\计算机\参考书\可能会读的书\计算机系统\操作系统概念\操作系统概念9edCN.pdf"),
-    "动手学机器学习": (r'E:\学习资料\计算机\参考书\可能会读的书\机器学习\动手学机器学习\动手学机器学习.mm',
-                       r'E:\学习资料\计算机\参考书\可能会读的书\机器学习\动手学机器学习\d2l-zh.pdf'),
-    "机器学习": (r"E:\学习资料\计算机\参考书\可能会读的书\机器学习\机器学习\机器学习.mm",
-                 r"E:\学习资料\计算机\参考书\可能会读的书\机器学习\机器学习\机器学习.pdf"),
-    "Java核心技术卷1": (r"E:\学习资料\计算机\参考书\可能会读的书\Java\入门\Java核心技术\Java核心技术卷1.mm",
-                        r"E:\学习资料\计算机\参考书\可能会读的书\Java\入门\Java核心技术\Java核心技术·卷I11ed.pdf"),
-    "Java核心技术卷2": (r"E:\学习资料\计算机\参考书\可能会读的书\Java\入门\Java核心技术\Java核心技术卷2.mm",
-                        r"E:\学习资料\计算机\参考书\可能会读的书\Java\入门\Java核心技术\Java核心技术·卷II11ed.pdf"),
-    "Linux命令行大全": (r"E:\学习资料\计算机\参考书\可能会读的书\Linux\Linux命令行大全\Linux命令行大全.mm",
-                        r"E:\学习资料\计算机\参考书\可能会读的书\Linux\Linux命令行大全\Linux命令行大全2ed.pdf"),
-    '计算机视觉': (r"E:\学习资料\2022-2023第一学期\计算机视觉\计算机视觉.mm",
-                   r"E:\学习资料\2022-2023第一学期\计算机视觉\Szeliski_CVAABook_2ndEd.pdf"),
-    "LaTeX入门": (r"E:\学习资料\计算机\参考书\可能会读的书\LaTeX\LaTeX入门\LaTeX入门.mm",
-                  r"E:\学习资料\计算机\参考书\可能会读的书\LaTeX\LaTeX入门\LaTeX入门.pdf"),
-    "大数据": (r"E:\学习资料\小学期\大数据系统开发\大数据.mm", r"E:\学习资料\小学期\大数据系统开发\ppt\大数据系统开发-final.pdf"),
-}
+intersect_portion = 0.3
 acrobat_address = r"C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
 t1 = (urllib.request.quote(f'"{acrobat_address}" /A "page='), 'evince -i ')
 
