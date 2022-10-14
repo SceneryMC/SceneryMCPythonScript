2c4b03d1347f8abf33b8e793f35eecfe95852665
SceneryMC <1330827323@qq.com>
2022-10-14 23:38:18 +0800

    下载

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import os
+import shutil
 
-
-with open('eso_all_images_14814.txt') as f:
-    all = [s.strip() for s in f.readlines()]
-with open('eso_skipped.txt') as f:
-    skipped = [s.strip()[s.rfind('/')+1:-4] for s in f.readlines()]
-downloaded = set([s[:-4] for s in os.listdir(r"G:\收藏\图片\ESO")])
-
-# all.sort(key=lambda x: x.lower())
-# for i in range(len(all)-1):
-#     if all[i].lower() == all[i+1].lower():
-#         print(all[i], all[i+1])
-# skipped.sort(key=lambda x: x.lower())
-# for i in range(len(skipped)-1):
-#     if skipped[i].lower() == skipped[i+1].lower():
-#         print(skipped[i], skipped[i+1])
-
+address = r"G:\收藏\图片\ESO\others"
+folders = ["_", "alma", "dsc", "ehpa", "img", "max", "uhd", "vista", "vlt"]
+filelist = os.listdir(address)
+print(filelist)
+for t in folders:
+    filelist.remove(t)
+for s in os.listdir(address):
+    for t in folders:
+        if s[:len(t)] == t:
+            shutil.move(rf"{address}\{s}", rf"{address}\{t}")
