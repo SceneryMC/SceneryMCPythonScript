67ac040ebf1e368699871fd4f437e312e5a24d1c
SceneryMC <1330827323@qq.com>
2022-10-25 12:07:49 +0800

    随便写个小测试

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import os
-import shutil
+# import os
+# import shutil
 
-address = r"G:\收藏\图片\ESO\others"
-folders = ["_", "alma", "dsc", "ehpa", "img", "max", "uhd", "vista", "vlt"]
-filelist = os.listdir(address)
-print(filelist)
-for t in folders:
-    filelist.remove(t)
-for s in os.listdir(address):
-    for t in folders:
-        if s[:len(t)] == t:
-            shutil.move(rf"{address}\{s}", rf"{address}\{t}")
+# address = r"G:\收藏\图片\ESO\others"
+# folders = ["_", "alma", "dsc", "ehpa", "img", "max", "uhd", "vista", "vlt"]
+# filelist = os.listdir(address)
+# print(filelist)
+# for t in folders:
+#     filelist.remove(t)
+# for s in os.listdir(address):
+#     for t in folders:
+#         if s[:len(t)] == t:
+#             shutil.move(rf"{address}\{s}", rf"{address}\{t}")
+import random as rd
+
+ls_1 = [str(rd.randint(0, 9)) for _ in range(2000)]
+ls_2 = [str(rd.randint(0, 9)) for _ in range(2000)]
+i1 = int(''.join(ls_1))
+i2 = int(''.join(ls_2))
+print(i1)
+print(i2)
+print(i1 * i2)
+
