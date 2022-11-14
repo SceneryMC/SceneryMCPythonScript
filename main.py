60610995adadfd724af65b133d4e805ab30e29d0
SceneryMC <1330827323@qq.com>
2022-11-14 08:37:35 +0800

    随手同步下

--- main.py
+++ main.py
@@ -11,13 +11,17 @@
 #     for t in folders:
 #         if s[:len(t)] == t:
 #             shutil.move(rf"{address}\{s}", rf"{address}\{t}")
-import random as rd
+# import random as rd
+#
+# ls_1 = [str(rd.randint(0, 9)) for _ in range(2000)]
+# ls_2 = [str(rd.randint(0, 9)) for _ in range(2000)]
+# i1 = int(''.join(ls_1))
+# i2 = int(''.join(ls_2))
+# print(i1)
+# print(i2)
+# print(i1 * i2)
+import re
 
-ls_1 = [str(rd.randint(0, 9)) for _ in range(2000)]
-ls_2 = [str(rd.randint(0, 9)) for _ in range(2000)]
-i1 = int(''.join(ls_1))
-i2 = int(''.join(ls_2))
-print(i1)
-print(i2)
-print(i1 * i2)
+test = "fwiijfowiejofjiscreenqwdqwdqwd"
+print(re.search(r"screen.*", test).group())
 
