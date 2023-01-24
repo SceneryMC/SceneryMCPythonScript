50c154126aa5e01f10ce5c40d129ecd5b0c457ac
SceneryMC <1330827323@qq.com>
2023-01-25 01:46:42 +0800

    开销更少：删除已有list中的元素以避免重复listdir

--- image_duplicate_cleaner.py
+++ image_duplicate_cleaner.py
@@ -10,15 +10,12 @@ preferences = {
 }
 
 folders_listdir = os.listdir(root)
-need_update = dict(zip(folders_listdir, [True] * len(folders_listdir)))
 folders = {}
-for folder in itertools.combinations(folders_listdir, 2):
-    for i in range(2):
-        if need_update[folder[i]]:
-            folders[folder[i]] = set(os.listdir(rf"{root}\{folder[i]}"))
-            need_update[folder[i]] = False
-    duplicate = folders[folder[0]].intersection(folders[folder[1]])
 
+for folder in folders_listdir:
+    folders[folder] = set(os.listdir(rf"{root}\{folder}"))
+for folder in itertools.combinations(folders_listdir, 2):
+    duplicate = folders[folder[0]].intersection(folders[folder[1]])
     if len(duplicate) != 0:
         if folder not in preferences:
             print(f"{folder}: 新增重复对，请定义优先级！")
@@ -26,8 +23,8 @@ for folder in itertools.combinations(folders_listdir, 2):
             folder_delete = folder[preferences[folder]]
             for file in duplicate:
                 os.remove(rf"{root}\{folder_delete}\{file}")
+                folders[folder_delete].remove(file)
                 print(rf"{root}\{folder_delete}\{file}")
-            need_update[folder_delete] = True
 
 files = []
 for _, _, file in os.walk(root):
