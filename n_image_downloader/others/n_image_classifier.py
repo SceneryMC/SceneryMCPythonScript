3d311231e583dd5068cd633e537296d5b433f1d1
SceneryMC <1330827323@qq.com>
2023-02-08 00:37:17 +0800

    双系统适配

--- n_image_downloader/others/n_image_classifier.py
+++ n_image_downloader/others/n_image_classifier.py
@@ -2,26 +2,26 @@ import os
 import shutil
 from path_Windows_to_Linux import *
 
-direct_file_path = path_Windows_to_Linux(r'F:\存储\其它\SYNC\等待分类\新建文件夹 (6)\新建 文本文档.txt')
-n_images_path = path_Windows_to_Linux(r'F:\存储\其它\SYNC\等待分类\新建文件夹 (6)')
+direct_file_path = path_Windows_to_Linux(r'C:\Users\SceneryMC\PycharmProjects\PythonScript\n_image_downloader\n_site.txt')
+n_images_path = path_Windows_to_Linux(r'C:\Users\SceneryMC\Downloads\图片助手(ImageAssistant)_批量图片下载器\n')
 
 with open(direct_file_path) as f:
     ls = [[y.lstrip('#') for y in x.split()] for x in f.readlines()]
 
 class_num = 0
-class_folder = f"{n_images_path}/{class_num}"
+class_folder = rf"{n_images_path}{os.sep}{class_num}"
 os.mkdir(class_folder)
 folder_list = os.listdir(n_images_path)
 for subclass in ls:
     if not subclass:
         print(f"第{class_num}类完成！")
         class_num += 1
-        class_folder = f"{n_images_path}/{class_num}"
+        class_folder = rf"{n_images_path}{os.sep}{class_num}"
         os.mkdir(class_folder)
         continue
     for folder in subclass:
         if folder in folder_list:
-            shutil.move(f"{n_images_path}/{folder}", f"{n_images_path}/{class_num}", copy_function=shutil.copy)
+            shutil.move(rf"{n_images_path}{os.sep}{folder}", rf"{n_images_path}{os.sep}{class_num}", copy_function=shutil.copy)
         else:
             print(f"{folder}缺失！")
 print(f"第{class_num}类完成，终止！")
