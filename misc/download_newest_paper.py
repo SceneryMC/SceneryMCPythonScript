be87b3014a0768a562ab44d0dd3e5df375951eb5
SceneryMC <1330827323@qq.com>
2023-03-28 22:28:28 +0800

    下载最新的paper-version-num.jar

--- misc/download_newest_paper.py
+++ misc/download_newest_paper.py
@@ -14,10 +14,11 @@ with os.popen(f"curl -X 'GET' \
 latest_build_json = result['builds'][-1]
 num, name = latest_build_json['build'], latest_build_json['downloads']['application']['name']
 old_version = [s for s in os.listdir(minecraft_server_path) if 'paper-' in s][0]
-if old_version != latest_version and False:
+if old_version != name:
     os.remove(f"{minecraft_server_path}/{old_version}")
     os.system(f"curl -X 'GET' \
       'https://api.papermc.io/v2/projects/paper/versions/{latest_version}/builds/{num}/downloads/{name}' \
       -H 'accept: application/json' \
       --output '{minecraft_server_path}/paper-{latest_version}-{num}.jar'")
     os.system(f"nautilus {minecraft_server_path}")
+    exit(0)
