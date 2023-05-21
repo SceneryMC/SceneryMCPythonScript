a5dad4e3cc7ca2bc47001aa5634fdf7e455a2362
SceneryMC <1330827323@qq.com>
2023-05-21 19:53:40 +0800

    双系统适配完成

--- pdf_page_and_annot_linker/bookxnote_to_freeplane.py
+++ pdf_page_and_annot_linker/bookxnote_to_freeplane.py
@@ -1,4 +1,5 @@
 import json
+import shutil
 import freeplane
 import lxml.etree as ET
 import os
@@ -37,13 +38,13 @@ class BooxnoteToFreeplane:
     text_dict = {"ref": "originaltext", "nonref": "content"}
 
 
-    def __init__(self, pdf_path, mm_path, json_path, pdf_name=None):
+    def __init__(self, pdf_path, mm_path, json_parent_path, pdf_name=None):
         bookxnote_pdf_name = os.path.basename(pdf_path)[:-4]
         self.pdf_path = pdf_path
         self.mm = freeplane.Mindmap()
         self.mm_path = mm_path
         self.pdf_name = pdf_name if pdf_name is not None else bookxnote_pdf_name
-        self.json_path = json_path
+        self.json_parent_path = json_parent_path
         self.t2 = generate_t2(pdf_path)
 
     def add_a_node(self, object, parent_node):
@@ -61,6 +62,8 @@ class BooxnoteToFreeplane:
             for d in object["annotations"]:
                 if d["style"] == 1:
                     node.set_image(link=f'{self.pdf_name}_files/{d["content"]}', size=0.6)
+                    shutil.copy(f"{self.json_parent_path}/imgfiles/{d['content']}",
+                                f"{os.path.dirname(self.mm_path)}/{self.pdf_name}_files")
                 elif d["style"] == 0 and node_type == "nonref":
                     add_detail(node, d["content"])
         return node
@@ -77,7 +80,7 @@ class BooxnoteToFreeplane:
 
 
     def translate(self):
-        with open(self.json_path, encoding='utf-8') as f:
+        with open(f"{self.json_parent_path}/markups.json", encoding='utf-8') as f:
             j = json.load(f)
         self.json_to_freeplane(j['markups'], self.mm.rootnode)
         self.mm.save(self.mm_path, encoding='utf-8')
@@ -86,7 +89,7 @@ class BooxnoteToFreeplane:
 if __name__ == '__main__':
     mm, pdf, _ = filelist['C++Primer']
     t = BooxnoteToFreeplane(path_Windows_to_Linux(pdf),
-                            path_Windows_to_Linux(r"E:\学习资料\计算机\参考书\可能会读的书\C++\入门\C++Primer\test.mm"),
-                            path_Windows_to_Linux(r"E:\学习资料\bookxnote\notebooks\C++Primer5edCN\markups.json"),
+                            path_Windows_to_Linux(r"E:\学习资料\2022-2023第二学期\大数据可视化\作业\2\test.mm"),
+                            path_Windows_to_Linux(r"E:\学习资料\bookxnote\notebooks\C++Primer5edCN"),
                             "C++Primer")
     t.translate()
