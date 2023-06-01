bb9c65356fd3acf9fc16e8245954b23233fe6b2e
SceneryMC <1330827323@qq.com>
2023-06-01 14:54:50 +0800

    无

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# import os
+# import shutil
+# import random
+# import fitz
+import chardet
+
 # new_lines = []
 # with open(r"C:\Users\SceneryMC\Source\Repos\assembly\first_window.asm") as f:
 #     lines = f.readlines()
@@ -12,9 +18,6 @@
 # with open(r"C:\Users\SceneryMC\Source\Repos\assembly\first_window.asm", 'w') as f:
 #     f.writelines(new_lines)
 
-# import os
-# import shutil
-
 
 # address = r"G:\收藏\图片\ESO\others"
 # folders = ["_", "alma", "dsc", "ehpa", "img", "max", "uhd", "vista", "vlt"]
@@ -28,16 +31,13 @@
 #             shutil.move(rf"{address}\{s}", rf"{address}\{t}")
 
 
-# import random as rd
-#
-# ls_1 = [str(rd.randint(0, 9)) for _ in range(2000)]
-# ls_2 = [str(rd.randint(0, 9)) for _ in range(2000)]
+# ls_1 = [str(random.randint(0, 9)) for _ in range(2000)]
+# ls_2 = [str(random.randint(0, 9)) for _ in range(2000)]
 # i1 = int(''.join(ls_1))
 # i2 = int(''.join(ls_2))
 # print(i1)
 # print(i2)
 # print(i1 * i2)
-# import random
 #
 # dis = ['Ubuntu', 'Fedora', 'ArchLinux', 'SUSE']
 #
@@ -46,16 +46,12 @@
 #         f.write(f"{random.choice(dis)}\t{random.randint(0, 12)}\t{random.randint(0, 12)}\t"
 #                 f"{random.randint(1, 28)}/{random.randint(1, 12)}/{random.randint(2000, 2023)}\n")
 
-# import fitz
-#
-#
 # doc = fitz.open('/mnt/E/学习资料/计算机/参考书/可能会读的书/C++/入门/C++Primer/C++Primer5edCN（复件）.pdf')
 # for page in doc:
 #     for annot in page.annots():
 #         if annot.colors['stroke'] == (1.0, 1.0, 0.0):
 #             annot.set_opacity(0.05)
 # doc.save('/mnt/E/学习资料/计算机/参考书/可能会读的书/C++/入门/C++Primer/C++Primer5edCN-tmp.pdf')
-# import os
 #
 # base = r'C:\Users\SceneryMC\Source\Repos\Project2\test1'
 #
@@ -73,7 +69,6 @@
 #     print(time[-1] - time[0])
 #     n, t = state.count('NEW'), state.count('TO')
 #     print(len(state), n, t, n / (n + t))
-import chardet
 
 
 with open(r"E:\学习资料\2022-2023第二学期\大数据可视化\项目\MC1\V1\新建 文本文档.txt", 'rb') as f:
