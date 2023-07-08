1595caf51a383d9832f11d47f1ad88fc0acbd0cc
SceneryMC <1330827323@qq.com>
2023-07-09 01:29:20 +0800

    划分函数更加规范，方便其它模块复用

--- n_image_downloader/n_image_downloader_fixed.py
+++ n_image_downloader/n_image_downloader_fixed.py
@@ -7,81 +7,107 @@ import os
 import time
 import json
 
+global driver, d_last, d_all
 path = r'C:\Users\SceneryMC\Downloads\图片助手(ImageAssistant)_批量图片下载器\n'
+test_url = f"https://nhentai.net/g/400000"
+last_log = 'last_n_site.json'
+all_log = 'all_n_site.json'
+download_list_file = 'n_site.txt'
 
 
 def get_basic_info(url):
     driver.get(url)
-    s = driver.page_source
+    src = driver.page_source
 
-    artist = re.search(r'<span class="tags"><a href="/artist/([^/]+)/"', s)
-    parodies = re.search('/parody/([^/]+)/', s)
+    artist = re.search(r'<span class="tags"><a href="/artist/([^/]+)/"', src)
+    parodies = re.search('/parody/([^/]+)/', src)
     return {'artist': artist.group(1) if artist else None,
-            'tags': re.findall(r'"/tag/([^/]+)/"', s),
-            'characters': re.findall(r'"/character/([^/]+)/"', s),
+            'tags': re.findall(r'"/tag/([^/]+)/"', src),
+            'characters': re.findall(r'"/character/([^/]+)/"', src),
             'parodies': parodies.group(1) if parodies else None,
             }, int(
                 re.search(r'<span class="name">(\d+)</span></a></span></div><div class="tag-container field-name">',
-                          s).group(
+                          src).group(
                     1))
 
 
-def get_images(serial, download):
-    url = f"https://nhentai.net/g/{serial}"
+def generate_url(work):
+    return f"https://nhentai.net/g/{work}"
+
+
+def visit_work(work, download):
+    url = generate_url(work)
     d, n = get_basic_info(url)
+    print(f'{url}: n = {n}')
 
-    print(f'{url}开始下载！n = {n}')
     if download:
-        address_temp = rf"{path}\{serial}"
-        if not os.path.exists(address_temp):
-            os.mkdir(address_temp)
+        download_images(work, n)
 
-        while True:
-            try:
-                driver.get(f"{url}/1")
-                s = driver.page_source
-                pattern = re.search(r'<img src="https://i(\d)\.nhentai\.net/galleries/(\d+)/\d+\.(jpg|png|gif)', s)
-                server, inner_serial = pattern.group(1), pattern.group(2)
-                break
-            except:
-                time.sleep(5)
-                print("VPN DOWN!")
-        folder = f"{base_url_pre}{server}{base_url_suf}/{inner_serial}"
-        while len(dir_ls := os.listdir(address_temp)) != n:
-            ls_download = set(range(1, n + 1)) - {int(x.split('.')[0]) for x in dir_ls}
-            print(ls_download)
+    d_last[work] = d_all[work] = d
+    save_log()
+    print(f'{url}完成！')
 
-            p = Pool()
-            for i in ls_download:
-                p.apply_async(temp_get_image, args=(i, serial, folder, address_temp,))
-            p.close()
-            p.join()
 
-    d_last[serial] = d_all[serial] = d
-    with open("last_n_site.json", 'w') as f:
+def download_images(work, n):
+    address_temp = rf"{path}\{work}"
+    if not os.path.exists(address_temp):
+        os.mkdir(address_temp)
+
+    while True:
+        try:
+            driver.get(f"{generate_url(work)}/1")
+            s = driver.page_source
+            pattern = re.search(r'<img src="https://i(\d)\.nhentai\.net/galleries/(\d+)/\d+\.(jpg|png|gif)', s)
+            server, inner_serial = pattern.group(1), pattern.group(2)
+            break
+        except:
+            time.sleep(5)
+            print("VPN DOWN!")
+    folder = f"{base_url_pre}{server}{base_url_suf}/{inner_serial}"
+    while len(dir_ls := os.listdir(address_temp)) != n:
+        ls_download = set(range(1, n + 1)) - {int(x.split('.')[0]) for x in dir_ls}
+        print(ls_download)
+
+        p = Pool()
+        for i in ls_download:
+            p.apply_async(temp_get_image, args=(i, work, folder, address_temp,))
+        p.close()
+        p.join()
+
+
+def save_log():
+    with open(last_log, 'w') as f:
         json.dump(d_last, f, ensure_ascii=False, indent=True)
-    with open('all_n_site.json', 'w') as f:
+    with open(all_log, 'w') as f:
         json.dump(d_all, f, ensure_ascii=False, indent=True)
-    print(f'{url}下载完成！')
 
 
-if __name__ == '__main__':
-    download = input("是否下载？") != "False"
+def init_driver():
+    global driver
     options = webdriver.ChromeOptions()
     options.headless = False
     driver = uc.Chrome(options=options)
     driver.set_window_size(192, 168)
-    driver.get(f"https://nhentai.net/g/400000")
+    driver.get(test_url)
     time.sleep(30)
 
-    with open("last_n_site.json") as f:
+
+def load_log():
+    global d_last, d_all
+    with open(last_log) as f:
         d_last = json.load(f)
-    with open('all_n_site.json') as f:
+    with open(all_log) as f:
         d_all = json.load(f)
-    with open("n_site.txt") as f:
+
+
+if __name__ == '__main__':
+    init_driver()
+    load_log()
+
+    with open(download_list_file) as f:
         content = re.findall("(\d{1,6})", f.read())
     for s in content:
         if s not in d_last:
-            get_images(s, download)
+            visit_work(s, True)
 
     driver.close()
