494aa717e29413507bc6416dd4c581e5b748f111
SceneryMC <1330827323@qq.com>
2023-07-24 23:04:52 +0800

    写点注释（真难写啊……）

--- pdf_page_and_annot_linker/pdf_page_and_annot_linker.py
+++ pdf_page_and_annot_linker/pdf_page_and_annot_linker.py
@@ -15,27 +15,21 @@ from error_correction_dictionary import character_error_correction, word_error_c
 from path_cross_platform import *
 from utils import *
 
+# 相交比：设一个词在pdf中的范围为x，选区y，若x & y的面积 > intersect_portion * min(x, y)则认为该词属于y
 intersect_portion = 0.3
+# image图片像素数到pdf导出的各个选区对应的坐标的换算，应该和image的dpi有关，这里直接写死
 length_to_pixel = 2.05
+# 别动就完事了
 mat = fitz.Matrix(2, 2)
 ns_re = {"re": "http://exslt.org/regular-expressions"}
 
 
 def error_correction(s):
+    # 按字替换与按词替换
     s = s.translate(str.maketrans(character_error_correction))
     for original, correction in word_error_correction.items():
         s = s.replace(original, correction)
     return s
-
-
-def get_args(args):
-    if args[2] in filelist:
-        mm_path, pdf_path, image_size = filelist[args[2]]
-    else:
-        mm_path = args[2]
-        pdf_path = args[3]
-        image_size = float(args[4])
-    return args[1], mm_path, pdf_path, image_size
 
 
 def mm_open_as_xml(mm_path: str) -> etree._ElementTree:
@@ -46,6 +40,7 @@ def mm_open_as_xml(mm_path: str) -> etree._ElementTree:
 
 
 def generate_command(pdf_path: str, page_num: int|str, pf=platform) -> str:
+    # 生成跳转到pdf的命令，Window必须用quote
     command = command_template[pf].replace("PAGE_NUM", str(page_num)).replace("PDF_PATH", path_fit_platform(pdf_path, pf))
     if pf == "Windows":
         command = urllib.request.quote(command)
@@ -65,6 +60,7 @@ def get_hightlighted_text(sep, annot, wordlist) -> str:
     words = []
     points = annot.vertices
     quad_count = len(points) // 4
+    # 按Quad判断wordlist中的词是否属于选区
     for i in range(quad_count):
         r = fitz.Quad(points[i * 4: i * 4 + 4]).rect
         for w in wordlist:
@@ -84,7 +80,7 @@ class PDFAnnotationLinker:
         self.pdf_path = pdf_path
         # 分词符，中文不存在，英文为空格。约定PDF主名结尾为CN表示为中文，否则为英文
         self.sep = '' if pdf_path[:-4].endswith("CN") else ' '
-        # freeplane存储图片的文件夹默认名
+        # freeplane存储图片的文件夹的默认名
         self.image_folder = f"{self.mm_name[:-3]}_files"
         # text 或 image，前者适合自带文字层的PDF，使用文字层内容；后者适合OCR生成的文字层质量不高的PDF，直接截图
         self.mode = mode
@@ -95,8 +91,8 @@ class PDFAnnotationLinker:
         self.mm = mm_open_as_xml(mm_path)
 
     def get_new_endpoints(self) -> list[etree._Element]:
-        # 不同时retrun self.mm.xpath(f".//*[re:match(text(), '{endpoint_regex}')]", namespaces=ns_re)的结果：不允许添加样式
         # 查找没有样式（否则text不为tag，而为element.text）、没有添加LINK的新节点
+        # 不同时retrun self.mm.xpath(f".//*[re:match(text(), '{endpoint_regex}')]", namespaces=ns_re)的结果：不允许添加样式
         return self.mm.xpath(f".//*[re:match(@TEXT, '{endpoint_regex}') and not(@LINK)]", namespaces=ns_re)
 
     def link_endpoints(self) -> None:
@@ -111,6 +107,7 @@ class PDFAnnotationLinker:
 
     def link_full_endpoint(self, endpoint: etree._Element, page_num: int, annot_num: int) -> None:
         print(f"P{page_num + 1}-{annot_num + 1}")
+        # 不同mode调用不同的函数
         method_map = {"text": self.change_text, "image": self.add_image}
 
         page = self.pdf[page_num]
@@ -119,10 +116,14 @@ class PDFAnnotationLinker:
         annot = next(itertools.islice(page.annots(types=[fitz.PDF_ANNOT_HIGHLIGHT]), annot_num, None))
         annot_text = error_correction(annot.info['content'])
 
+        # 调用mode对应的函数，text添加文字，image创建截图
         method_map[self.mode](page, annot, endpoint)
+        # 将pdf高亮区域保存到mm中，以便转换回bookxnote
         save_vertices(endpoint, annot.vertices)
 
+        # 添加跳转命令（method_map中不含此步）
         endpoint.set("LINK", f"execute:_{command}")
+        # 添加节点明细
         if annot_text != '':
             detail = etree.fromstring(f'<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">\n'
                                       f'<html>\n\t<head>\n\n\t</head>\n\t<body>\n\t\t<p>{html.escape(annot_text)}</p>'
@@ -138,19 +139,24 @@ class PDFAnnotationLinker:
         endpoint.set("TEXT", f'{endpoint.get("TEXT")} {html.escape(highlight)}"')
 
     def add_image(self, page, annot, endpoint: etree._Element) -> None:
+        # annot.vertices内容：[(x0, y0), ...]
+        # 获取四个边界的位置
         x = [p[0] for p in annot.vertices]
         y = [p[1] for p in annot.vertices]
         l, u, r, b = min(x), min(y), max(x), max(y)
 
+        # 由边界创建白背景（否则为空背景）
         plane = Image.new('RGB', (int((r - l) * length_to_pixel), int((b - u) * length_to_pixel)), color="white")
         points = annot.vertices
         quad_count = len(points) // 4
+        # 将文字按Quad截图，粘贴到白背景上
         for i in range(quad_count):
             clip = fitz.Quad(points[i * 4: i * 4 + 4]).rect
             pix = page.get_pixmap(matrix=mat, clip=clip, annots=False)
             plane.paste(Image.frombytes('RGB', (pix.width, pix.height), pix.samples),
                         (int((clip.x0 - l) * length_to_pixel), int((clip.y0 - u) * length_to_pixel)))
 
+        # 生成图片文件，并在当前节点上添加该图片
         png_filename = f"png_{random.randrange(0, 0xffffffffffff)}.png"
         img_tag = etree.fromstring(f'<hook URI="{self.image_folder}/{png_filename}" SIZE="{image_size}" '
                                    f'NAME="ExternalObject"/>')
