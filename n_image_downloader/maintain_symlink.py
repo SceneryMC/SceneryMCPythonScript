b28148af2042d5d48dbaf93ef72096c59aa18a48
SceneryMC <1330827323@qq.com>
2023-12-12 14:57:44 +0800

    添加新的标签

--- n_image_downloader/maintain_symlink.py
+++ n_image_downloader/maintain_symlink.py
@@ -9,7 +9,7 @@ classifiers = {
     'characters': {'cirno', 'flandre-scarlet', 'remilia-scarlet', 'patchouli-knowledge', 'reimu-hakurei', 'sakuya-izayoi'
                    'marisa-kirisame', 'yukari-yakumo', 'sanae-kochiya', 'momiji-inubashiri', 'yuuka-kazami',
                    'tenshi-hinanai', 'nue-houjuu', 'satori-komeiji', 'madoka-higuchi', 'yuuka-hayase', 'rin-tosaka', 'neeko',
-                   'xiangling', 'hu-tao'},
+                   'xiangling', 'hu-tao', 'furina', 'focalors'},
     'parodies': {'touhou-project', 'genshin-impact', 'arknights', 'blue-archive', 'league-of-legends'}
 }
 map_classifier_to_folder = {'tags': "TAG", "characters": "CHARACTER", 'parodies': "PARODY"}
