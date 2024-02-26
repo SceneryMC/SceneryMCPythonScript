05ab2e9084dfe5f43eaf29adcbf81177ffb6ac1b
SceneryMC <1330827323@qq.com>
2024-02-26 23:55:23 +0800

    迁移完成！

--- misc/image_resizer.py
+++ misc/image_resizer.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 from path_cross_platform import path_fit_platform
 
-base = path_fit_platform(r"F:\存储\其它\outputs\outputs\genshin\xiangling\3\4\best\C")
+base = path_fit_platform(r"E:\共享\kdetmp")
 dest = path_fit_platform(r"E:\共享\dst")
 
 
